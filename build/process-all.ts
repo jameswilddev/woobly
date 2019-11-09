b3cd2e21e602b5ce370fd20acfcea0d38fb3518d
jameswilddev <jameswilddev@outlook.com>
2019-11-09 00:22:13 +0000

    Ensure that stub directory exists.

--- build/process-all.ts
+++ build/process-all.ts
@@ -2,6 +2,8 @@ import * as fs from "fs"
 import processPackage from "./process-package"
 
 export default async function (): Promise<void> {
+  console.log(`Ensuring that directory exists for stub package...`)
+  await fs.promises.mkdir(`woobly`, { recursive: true })
   await processPackage([`woobly`])
 
   console.log(`Checking namespaced packages...`)
