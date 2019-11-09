a6f761cdc8328485e50c69842f9ebb595531617c
jameswilddev <jameswilddev@outlook.com>
2019-11-09 00:07:56 +0000

    Add "woobly" stub package.

--- build/process-all.ts
+++ build/process-all.ts
@@ -2,6 +2,8 @@ import * as fs from "fs"
 import processPackage from "./process-package"
 
 export default async function (): Promise<void> {
+  await processPackage([`woobly`])
+
   console.log(`Checking namespaced packages...`)
   for (const name of await fs.promises.readdir(`@woobly`)) {
     await processPackage([`@woobly`, name])
