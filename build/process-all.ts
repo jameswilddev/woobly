505999b1f513da4ae75a35e29f3361e009d91798
jameswilddev <jameswilddev@outlook.com>
2019-11-10 17:38:39 +0000

    Remove check which is no longer necessary as there is always a file in the directory being created.

--- build/process-all.ts
+++ build/process-all.ts
@@ -3,8 +3,6 @@ import processPackage from "./process-package"
 import writeRootReadme from "./write-root-readme"
 
 export default async function (): Promise<void> {
-  console.log(`Ensuring that directory exists for stub package...`)
-  await fs.promises.mkdir(`woobly`, { recursive: true })
   await processPackage([`woobly`])
 
   console.log(`Checking namespaced packages...`)
