6b52be6db9fc7108afe20d6dba61dd219006fe44
jameswilddev <jameswilddev@outlook.com>
2019-11-11 22:47:57 +0000

    Refactor generation of Markdown tables.

--- build/generate-root-readme-package-table.ts
+++ build/generate-root-readme-package-table.ts
@@ -1,18 +1,22 @@
 import allPackages from "./all-packages"
+import generateMarkdownTable from "./generate-markdown-table"
 
 export default function (): string {
-  const packageTable = allPackages
-    .sort((a, b) => a.name.localeCompare(b.name))
-    .map(p => [`[${p.name}](${p.name})`, `[![${p.version}](https://img.shields.io/npm/v/${p.name}.svg)](https://www.npmjs.com/package/${p.name})`, p.description])
-
-  packageTable.unshift([`Name`, `Version`, `Description`])
-
-  const longestOfColumn = packageTable[0]
-    .map((_, i) => Math.max.apply(Math, packageTable.map(row => row[i].length)))
-
-  packageTable.splice(1, 0, longestOfColumn.map(length => `-`.repeat(length)))
 
   return `## NPM packages
 
-${packageTable.map(row => row.map((column, i) => column.padEnd(longestOfColumn[i], ` `)).join(` | `)).join(`\n`)}`
+${generateMarkdownTable(
+    [
+      [`subdirectoryLink`, `Name`],
+      [`npmLink`, `Version`],
+      [`description`, `Description`],
+    ],
+    `name`,
+    allPackages.map(p => ({
+      name: p.name,
+      subdirectoryLink: `[${p.name}](${p.name})`,
+      npmLink: `[![${p.version}](https://img.shields.io/npm/v/${p.name}.svg)](https://www.npmjs.com/package/${p.name})`,
+      description: p.description
+    }))
+  )}`
 }
